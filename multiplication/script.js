f7a5fd56f08d560bbc5df839ca485520641ebbb6
MagOss27 <magnusoss98@gmail.com>
2023-11-20 23:57:46 -0300

    Criação do multiplication

---
+++ multiplication/script.js
@@ -0,0 +1,9 @@
+let Primeiro_numero
+let Segundo_numero
+let Multi_numeros
+
+Primeiro_numero = Number (prompt("Digite o Primeiro Número: "))
+Segundo_numero = Number (prompt("Digite o Segundo Número: "))
+
+Multi_numeros = Number(Primeiro_numero * Segundo_numero)
+alert (`O resultado da Multiplicação é ${Multi_numeros}`)
