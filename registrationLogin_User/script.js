748bbf8fe00b1a35fdef030c4be81280c1bc12e0
Magnus Oss <magnus.oss@reivax.com>
2023-12-28 09:41:09 -0300

    Criação do registrationLogin_User

--- registrationLogin_User/script.js
+++ registrationLogin_User/script.js
@@ -1,95 +1,112 @@
+// Obtendo referências aos elementos do DOM para cadastro
 let nomeCadastro = document.getElementById('userCadastro');
 let emailCadastro = document.getElementById('emailCadastro');
 let senhaCadastro = document.getElementById('senhaCadastro');
 
-// Armazena os valores dos inputs da tela de Login
+// Obtendo referências aos elementos do DOM para login
 let emailLogin = document.getElementById('emailLog'); // Nome alterado para evitar conflitos
 let senhaLogin = document.getElementById('senhaLog');
 
-// Cria vetores vazios para armazenamento temporário dos dados
-let nomes = []
-let emails = []
-let senhas = []
+// Vetores vazios para armazenamento temporário dos dados
+let nomes = [];
+let emails = [];
+let senhas = [];
 
 function cadastrarUser() {
-
-    // Obtém os valores dos campos de entrada
+    
+    // Obtendo os valores dos campos de entrada
+    /*O método Trim() remove espaços em branco adiconal no início ou no final desses valores seja removido,
+    o que pode ser útil para evitar erros ou inconsistências nos dados inseridos pelo usuário*/
     let nome = nomeCadastro.value.trim();
     let email = emailCadastro.value.trim();
     let senha = senhaCadastro.value.trim();
 
-    // Verifica se algum campo está vazio
+    // Verificando se algum campo está vazio
     if (!nome || !email || !senha) {
         alert("Por favor, preencha todos os campos de cadastro.");
         return; // Impede o cadastro se algum campo estiver vazio
     }
 
-    // Pega valores do LocalStorage (se tiver) e armazena
-    let nomes = JSON.parse(localStorage.getItem('Cadastro_User')) || [];
-    let emails = JSON.parse(localStorage.getItem('Cadastro_Email')) || [];
-    let senhas = JSON.parse(localStorage.getItem('Cadastro_password')) || [];
+    // Obter valores do LocalStorage (se houver) e armazenar
+    let nomesLocalStorage = JSON.parse(localStorage.getItem('Cadastro_User')) || [];
+    let emailsLocalStorage = JSON.parse(localStorage.getItem('Cadastro_Email')) || [];
+    let senhasLocalStorage = JSON.parse(localStorage.getItem('Cadastro_password')) || [];
 
-    // Adiciona os valores dos inputs aos vetores
-    nomes.push(nome);
-    emails.push(email);
-    senhas.push(senha);
+    // Adicionar os valores dos inputs aos vetores
+    nomesLocalStorage.push(nome);
+    emailsLocalStorage.push(email);
+    senhasLocalStorage.push(senha);
 
-    // Joga para o LocalStorage novamente
-    localStorage.setItem('Cadastro_User', JSON.stringify(nomes));
-    localStorage.setItem('Cadastro_Email', JSON.stringify(emails));
-    localStorage.setItem('Cadastro_password', JSON.stringify(senhas));
+    // Salvar no LocalStorage novamente
+    localStorage.setItem('Cadastro_User', JSON.stringify(nomesLocalStorage));
+    localStorage.setItem('Cadastro_Email', JSON.stringify(emailsLocalStorage));
+    localStorage.setItem('Cadastro_password', JSON.stringify(senhasLocalStorage));
 
-    // Mostra mensagem cadastro efetuado e carrega a página de login
+    // Mostrar mensagem de cadastro efetuado e redirecionar para a página de login
     alert("Seu cadastro foi efetuado com sucesso!");
-    // Pula para a página de Login
-    window.location.href = "login.html";
-    
+    // Redirecionar para a página de Login
+    window.location.href = "../telaLogin/login.html";
 }
 
 function logarUser() {
+    // Obter valores do LocalStorage (se houver) e armazenar
+    let emailsLocalStorage = JSON.parse(localStorage.getItem('Cadastro_Email')) || [];
+    let senhasLocalStorage = JSON.parse(localStorage.getItem('Cadastro_password')) || [];
 
-    // Pega valores do LocalStorage (se tiver) e armazena
-    let emails = JSON.parse(localStorage.getItem('Cadastro_Email')) || [];
-    let senhas = JSON.parse(localStorage.getItem('Cadastro_password')) || [];
-
-    // Exibe os valores recuperados do LocalStorage
-    console.log('Emails no localStorage:', emails);
-    console.log('Senhas no localStorage:', senhas);
-
-    // Verifica se os dados existem no localStorage
-    if (emails.length === 0 || senhas.length === 0) {
+    // Verificar se os dados existem no LocalStorage
+    if (emailsLocalStorage.length === 0 || senhasLocalStorage.length === 0) {
         alert('Nenhum usuário cadastrado. Faça o cadastro primeiro.');
         return;
     }
 
-    // Exibe os valores inseridos no formulário de login
-    console.log('Emails do LocalStorage:', emails);
-    console.log('Senhas do LocalStorage:', senhas);
-    console.log('Email inserido:', emailLog.value);
-    console.log('Senha inserida:', senhaLogin.value);
-
     let logou = false;
 
-    // Realiza um loop do tamanho dos vetores de emails e senhas
-    for (let i = 0; i < emails.length; i++) {
-        // Se o email no input do login for encontrado nos emails armazenados
-        if (emailLog.value === emails[i] && senhaLogin.value === senhas[i]) {
-            // Indica que o login foi bem-sucedido
+    // Verificar se o email e a senha inseridos correspondem aos valores no localStorage
+    for (let i = 0; i < emailsLocalStorage.length; i++) {
+        if (emailLogin.value === emailsLocalStorage[i] && senhaLogin.value === senhasLocalStorage[i]) {
             logou = true;
             break;
         }
     }
 
     if (logou) {
-        // Mostra mensagem de login efetuado
         alert('Login efetuado!');
-        // Pula para a página Principal
-        window.location.href = 'principal.html';
+        // Redirecionar para a página Principal
+        window.location.href = '../telaPrincipal/principal.html';
     } else {
-        // Senão, mostra mensagem de login falhou
         alert('Login Falhou!');
     }
 }
 
 
+function togglePasswordCadastro() {
+    let senhaInput = document.getElementById('senhaCadastro');
+    let showPassword = document.getElementById('showPasswordCadastro');
 
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+        showPassword.textContent = '🔒';
+    } else {
+        senhaInput.type = 'password';
+        showPassword.textContent = '👁️';
+    }
+}
+
+function togglePasswordLogin() {
+    let senhaInput = document.getElementById('senhaLog');
+    let showPassword = document.getElementById('showPasswordCadastro');
+
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+        showPassword.textContent = '🔒';
+    } else {
+        senhaInput.type = 'password';
+        showPassword.textContent = '👁️';
+    }
+}
+
+// Função para logout
+function logout() {
+    localStorage.removeItem('isLoggedIn');
+    window.location.href = '../telaLogin/login.html';
+}
