c49f9c190a6549b37792269c520eb34ea8f45a8c
MAGNUS OSS <magnus_oss@estudante.sesisenai.org.br>
2024-04-03 16:29:43 -0300

    Criação do pacmanGame

--- pacmanGame/ghost.js
+++ pacmanGame/ghost.js
@@ -263,17 +263,6 @@ class Ghost {
             this.width,
             this.height
         );
-        canvasContext.restore();
-        canvasContext.beginPath();
-        canvasContext.strokeStyle = "red";
-        canvasContext.arc(
-            this.x + oneBlockSize / 2,
-            this.y + oneBlockSize / 2,
-            this.range * oneBlockSize,
-            0,
-            2 * Math.PI
-        );
-        canvasContext.stroke();
     }
 }
 
